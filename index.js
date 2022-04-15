ce93297ded39fcbce6338c2156a2af5c68d7a47f
Priscilla-Egberi <priscillaegberi@gmail.com>
2022-04-15 22:17:40 +0100

    added buttons

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const updates = Vue.createApp({
             books:[
                 { author:"Holy spirit", name: "The holy Bible", price:0, completed:true},
                 { author:"Chinua Achebe", name: "Things Fall Apart", price:5, completed:false},
-                { author:"Casandra Claire", name: "The Mortal Instrument", price:100, completed:true},
                 { author:"Ugo C. Ugo", name: "Past Questions", price:1, completed:false},
+                { author:"Casandra Claire", name: "The Mortal Instrument", price:100, completed:true},
                 { author:"Bright", name: "Brighter Grammar", price:160, completed:true},
                 { author:"Lucifer", name: "The devil's handbook", price:0, completed:false}
             ],
